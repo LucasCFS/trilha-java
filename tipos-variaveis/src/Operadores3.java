e7a95b3bc6520d9fc202b212e9dca1da556b3986
Lucas <lucascfs19@gmail.com>
2023-09-19 14:40:22 -0300

    atualização sobre as aulas spbre operadores.

---
+++ tipos-variaveis/src/Operadores3.java
@@ -0,0 +1,12 @@
+public class Operadores3 {
+    public static void main(String[] args) {
+        // inverter uma expressão boolean é "!"
+        
+        int numero = 5;
+       
+        numero++;
+
+        System.out.println(numero);
+
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+         /*  números primitivos 
+        //exemplo de string
+        
+       // String meuNome = "Lucas Cunha";
+
+       // short numeroCurto = 1;
+       // int numero = 5;
+       // final double PI = 3.14;
+       */
     }
 }
